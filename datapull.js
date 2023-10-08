4044875a5b059b8d48507cbfd5d3b207f91f7ff9
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-08 11:57:39 +0530

    Update datapull.js

--- datapull.js
+++ datapull.js
@@ -1,3 +1,5 @@
+// Replace 'YOUR_API_KEY' with your Google API Key
+const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
 const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
 const SHEET_NAME = 'Rank';
 
@@ -7,7 +9,7 @@ gapi.load('client', initClient);
 // Initialize the Google Sheets API client
 function initClient() {
     gapi.client.init({
-        apiKey: 'YOUR_API_KEY', // Replace with your API key
+        apiKey: API_KEY,
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(function() {
         // Call the function to fetch data
@@ -24,11 +26,59 @@ function fetchSheetData() {
         const values = response.result.values;
         if (values && values.length > 0) {
             // Process and display the data here
-            console.log(values);
+            const playerContainer = document.getElementById('playerContainer');
+            playerContainer.innerHTML = ''; // Clear existing content
+
+            values.forEach(function(row) {
+                const playerCard = document.createElement('div');
+                playerCard.classList.add('player-card');
+                playerCard.setAttribute('data-level', row[3]);
+                playerCard.setAttribute('data-name', row[1]);
+
+                const playerInfo = document.createElement('div');
+                playerInfo.classList.add('player-info');
+
+                const playerName = document.createElement('span');
+                playerName.classList.add('player-name');
+                playerName.textContent = row[0] + '. ' + row[1];
+
+                const playerCoins = document.createElement('span');
+                playerCoins.textContent = 'S+ Coins: ' + row[2];
+
+                const progressBar = document.createElement('div');
+                progressBar.classList.add('progress-bar');
+
+                const progressBarInner = document.createElement('div');
+                progressBarInner.classList.add('progress-bar-inner');
+                progressBarInner.style.width = (row[2] / 210) * 100 + '%';
+
+                progressBar.appendChild(progressBarInner);
+                playerInfo.appendChild(playerName);
+                playerInfo.appendChild(playerCoins);
+                playerCard.appendChild(playerInfo);
+                playerCard.appendChild(progressBar);
+                playerContainer.appendChild(playerCard);
+            });
         } else {
             console.log('No data found.');
         }
     }, function(response) {
         console.error('Error fetching data:', response.result.error.message);
     });
+}
+
+// Function to search and filter data
+function searchTable() {
+    var input, filter, cards, name, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    cards = document.getElementsByClassName("player-card");
+    for (i = 0; i < cards.length; i++) {
+        name = cards[i].getAttribute("data-name");
+        if (name.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
 }

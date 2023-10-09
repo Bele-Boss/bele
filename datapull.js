8a04466b796b9d6e7d92c02551d93111382fa088
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-09 21:25:23 +0530

    Update datapull.js

--- datapull.js
+++ datapull.js
@@ -57,17 +57,19 @@ function createPlayerCard(player) {
 
     progressBar.style.backgroundColor = progressBarColor;
 
-    // Calculate the width of the progress bar within its color segment
-    const colorMinCoins = [0, 11, 31, 61, 101, 151, 211];
-    const colorMaxCoins = [10, 30, 60, 100, 150, 210, 1000]; // Added a high max for black
-    let progressBarWidth = 0;
+   
+// Calculate the width of the progress bar within its color segment
+const colorMinCoins = [0, 11, 31, 61, 101, 151, 211];
+const colorMaxCoins = [10, 30, 60, 100, 150, 210, 1000]; // Added a high max for black
+let progressBarWidth = 0;
 
-    for (let i = 0; i < colorMinCoins.length; i++) {
-        if (coins >= colorMinCoins[i] && coins <= colorMaxCoins[i]) {
-            progressBarWidth = ((coins - colorMinCoins[i]) / (colorMaxCoins[i] - colorMinCoins[i] + 1)) * 100;
-            break;
-        }
+for (let i = 0; i < colorMinCoins.length; i++) {
+    if (coins >= colorMinCoins[i] && coins <= colorMaxCoins[i]) {
+        // Set a minimum width of 1% to ensure it's not completely empty
+        progressBarWidth = Math.max(((coins - colorMinCoins[i]) / (colorMaxCoins[i] - colorMinCoins[i] + 1)) * 100, 1);
+        break;
     }
+}
 
     progressBar.style.width = `${progressBarWidth}%`;
 

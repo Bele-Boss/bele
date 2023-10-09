fee5c4e19d3c8bc6a569e965453b076239b7eeb7
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-09 22:42:03 +0530

    Update datapull.js

--- datapull.js
+++ datapull.js
@@ -68,7 +68,7 @@ function createPlayerCard(player) {
     }
 
     if ([11, 31, 61, 101, 151, 211].includes(coins)) {
-        progressBarWidth = Math.max(progressBarWidth, 4); // Ensuring at least 4% width
+        progressBarWidth = Math.max(progressBarWidth, 2); // Ensuring at least 2% width
     }
 
     progressBar.style.width = `${progressBarWidth}%`;
